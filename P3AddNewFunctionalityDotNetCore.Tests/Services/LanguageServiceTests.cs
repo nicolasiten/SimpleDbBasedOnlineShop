f8a5b66f5b0c60bad3a5d6faad6c5eb51c5bfa11
Nicolas Iten <nici.iten@gmail.com>
2019-09-18 21:20:28 +0200

    Implemented LanguageServiceTests

---
+++ P3AddNewFunctionalityDotNetCore.Tests/Services/LanguageServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using P3AddNewFunctionalityDotNetCore.Models.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace P3AddNewFunctionalityDotNetCore.Tests.Services
+{
+    public class LanguageServiceTests
+    {
+        private readonly ILanguageService _languageService;
+
+        public LanguageServiceTests()
+        {
+            _languageService = new LanguageService();
+        }
+
+        [Theory]
+        [InlineData("English", "en")]
+        [InlineData("French", "fr")]
+        [InlineData("Spanish", "en")]
+        [InlineData("", "en")]
+        public void SetCultureTest(string language, string expectedResult)
+        {
+            string result = _languageService.SetCulture(language);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
